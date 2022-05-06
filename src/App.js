2a516087be3f0b75465dfbbb51b62acd3a5b1a73
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-05-06 17:18:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with kamal
         </a>
       </header>
     </div>
