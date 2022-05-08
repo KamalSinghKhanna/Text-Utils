ee1a8a5c0bf955eee57b855357736fab8650d076
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-05-08 19:59:14 +0530

    day2

---
+++ src/module1.mjs
@@ -0,0 +1,12 @@
+const a = 'harry';
+const b = "kamal";
+const c = "vansh";
+const d = "Rohan";
+const e = "luci";
+
+export default b;
+export {c}
+export {d}
+export {e}
+export {a}
+
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+import ui, {a,c,d,e} from './module1.mjs';
+console.log(a);
+console.log(ui);
+console.log(c);
+console.log(d);
+console.log(e);
