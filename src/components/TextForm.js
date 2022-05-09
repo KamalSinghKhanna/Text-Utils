fdf96e3227c1b5fa0a59a5ce93492858742bdabb
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-05-09 10:18:27 +0530

    adding logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,7 +6,7 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleDownClick = ()=>{
+    const handleLoClick = ()=>{
         // console.log("Uppercase was clicked " + text);
         let newText = text.toLowerCase();
         setText(newText);
@@ -16,18 +16,27 @@ export default function TextForm(props) {
         // console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // text = "this is new text"; // this is a wrong way
     // setText = "this is new text"; // this is a Correnct way
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-secondary" onClick={handleDownClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-secondary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
